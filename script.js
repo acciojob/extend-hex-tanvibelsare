9dd6f489b2cfbb88c2ab97a4fa2e97e3cbe7b457
tanvibelsare <118069064+tanvibelsare@users.noreply.github.com>
2023-06-10 12:56:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const extendHex = (shortHex) => {
   // write your code here
+	"#"+shortHex.slice(shortHex.startWith("#")? 1 : 0)
+	 .split('')
+    .map(x => x + x)
+    .join('');
+	return shortHex
 };
 
 // Do not change the code below.
